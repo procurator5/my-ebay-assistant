66e6a966d3216395e13a38e020143d188de97f65
Владимир Иванов <procurator9@gmail.com>
2017-12-12 16:05:22 +0300

    Добавил главную страницу для сайта

--- my_ebay_assistant/urls.py
+++ my_ebay_assistant/urls.py
@@ -19,6 +19,16 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib.auth.views import login
+from django.template import loader
+from django.http import HttpResponse
+from ebay_parse.models import eBayCategory
+
+def index(request):
+    template = loader.get_template("index.html")
+    context = {
+                'nodes': eBayCategory.objects.all(),
+                }
+    return HttpResponse(template.render(context, request))
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,7 +36,8 @@ urlpatterns = [
     url(r'^ebay_parse/', include('ebay_parse.urls')),
     url(r'^species/', include('species.urls')),
     url('^', include('django.contrib.auth.urls')),
-    url('^login/$', login, name='login')
+    url('^login/$', login, name='login'),
+    url(r'^$', index, name='index'),    
 ]
 
 # В конце файла:

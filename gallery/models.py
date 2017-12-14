cee803da7194e155cf12944208d149b8294bb5eb
Владимир Иванов <procurator9@gmail.com>
2017-12-14 18:08:07 +0300

    Автосоздание галереи для вида

--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from ebay_parse.models import eBayItemGallery
+from species.models import Species
+from reportlab.lib.colors import black
 
 # Create your models here.
 class Gallery(models.Model):
     gallery_name = models.CharField(max_length = 256)
     gallery_prop = models.TextField(blank = True)
+    species = models.ForeignKey('species.Species', on_delete=models.CASCADE, null = True, blank = True)
     
 class GalleryItem(models.Model):
     gallery = models.ForeignKey('Gallery', on_delete=models.CASCADE)    

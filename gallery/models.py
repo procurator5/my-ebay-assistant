a9a7613893b13294d756a8d1d35682760a8fb9a8
Владимир Иванов <procurator9@gmail.com>
2017-12-14 18:02:57 +0300

    Добавлены обьекты БД для хранения галерей

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ebay_parse.models import eBayItemGallery
 
 # Create your models here.
+class Gallery(models.Model):
+    gallery_name = models.CharField(max_length = 256)
+    gallery_prop = models.TextField(blank = True)
+    
+class GalleryItem(models.Model):
+    gallery = models.ForeignKey('Gallery', on_delete=models.CASCADE)    
+    gallery_item = models.ForeignKey('ebay_parse.eBayItemGallery', on_delete=models.CASCADE)
